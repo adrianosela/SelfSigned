da5d44e7f9a1df60566f959e2b0832d9ca3adb93
Adriano Sela Aviles <adriano.selaviles@gmail.com>
2023-10-06 11:34:39 -0700

    update to go 1.21

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adrianosela/selfsigned
+
+go 1.21
--- self-signed.go
+++ self-signed.go
@@ -1,34 +1,33 @@
 package main
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"io/ioutil"
 	"log"
 	"math/big"
-	random "math/rand"
+	"math/rand"
 	"os"
 	"time"
 )
 
+var randReader = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func main() {
 	generateSelfSignedCert(os.Args[1])
 }
-
 func generateSelfSignedCert(cn string) *tls.Certificate {
 	template := x509.Certificate{
-		SerialNumber: big.NewInt(random.New(random.NewSource(time.Now().UnixNano())).Int63()), //time seeded serial number
+		SerialNumber: big.NewInt(rand.Int63()), //time seeded serial number
 		Subject: pkix.Name{
 			CommonName:   cn,
 			Country:      []string{"CA"},
 			Organization: []string{"AdrianoSela Inc."},
 		},
-		NotBefore:             time.Now(),                                               //valid from now on
-		NotAfter:              time.Now().Add(time.Duration(time.Hour * 24 * 365 * 10)), //valid for 10 years
+		NotBefore:             time.Now().Add(time.Minute * -5),                         // valid from 5 minutes ago (allow for clock skews)
+		NotAfter:              time.Now().Add(time.Duration(time.Hour * 24 * 365 * 10)), // valid until 10 years from now
 		IsCA:                  true,
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
@@ -36,7 +35,7 @@ func generateSelfSignedCert(cn string) *tls.Certificate {
 	}
 
 	//generate a key pair
-	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	key, err := rsa.GenerateKey(randReader, 2048)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -45,7 +44,7 @@ func generateSelfSignedCert(cn string) *tls.Certificate {
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	})
 
-	certData, err := x509.CreateCertificate(rand.Reader, &template, &template, key.Public(), key)
+	certData, err := x509.CreateCertificate(randReader, &template, &template, key.Public(), key)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -59,11 +58,11 @@ func generateSelfSignedCert(cn string) *tls.Certificate {
 		log.Fatal(err)
 	}
 
-	if err = ioutil.WriteFile("key.pem", keyBytes, 0644); err != nil {
+	if err = os.WriteFile("key.pem", keyBytes, 0644); err != nil {
 		log.Fatal(err)
 	}
 
-	if err = ioutil.WriteFile("cert.pem", certBytes, 0644); err != nil {
+	if err = os.WriteFile("cert.pem", certBytes, 0644); err != nil {
 		log.Fatal(err)
 	}
 
